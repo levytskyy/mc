1515dc554f1535ae2d0b8e03ae2f3ef1e0982b2a
yurii <yuriy.levytskyy@gmail.com>
2019-06-07 11:12:05 +0300

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
+
+//scroll
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: false,
+  suppressScrollY: true,
+  useBothWheelAxes: true
+};
+
 
 @NgModule({
   declarations: [
@@ -13,6 +25,8 @@ import {TransferHttpCacheModule} from '@nguniversal/common';
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'my-app'}),
+    PerfectScrollbarModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full'},
       { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
@@ -20,7 +34,12 @@ import {TransferHttpCacheModule} from '@nguniversal/common';
     ]),
     TransferHttpCacheModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,101 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation, ViewChild, PLATFORM_ID, Inject, OnInit} from '@angular/core';
+import { trigger, transition, animate, style, state } from '@angular/animations'
+import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
+import { isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <h3>{{ message }}</h3>
-    <img [src]="image">
-  `
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  animations: [
+    trigger(
+        'enterAnimation', [
+          transition(':enter', [
+            style({transform: 'translateX(-100%)'}),
+            animate('250ms', style({transform: 'translate3d(0, 0, 0)'}))
+          ]),
+          transition(':leave', [
+            style({transform: 'translateX(0)', opacity: 1}),
+            animate('250ms', style({transform: 'translate3d(0, 650px, 0)'}))
+          ])
+        ]
+    )
+  ]
 })
-export class HomeComponent {
-  public message = `Angular Universal`;
-  // tslint:disable-next-line:max-line-length
-  public image = 'https://camo.githubusercontent.com/81f72f2fdf98aa1d30b5b215bc8ca9420b249e81/68747470733a2f2f616e67756c61722e696f2f67656e6572617465642f696d616765732f6d61726b6574696e672f636f6e636570742d69636f6e732f756e6976657273616c2e706e67';
 
-  constructor() {}
+export class HomeComponent implements OnInit{
+  @ViewChild('scroll') scrollEl: PerfectScrollbarComponent;
+  @ViewChild('panel') panelEl: any;
+  isBrowser:boolean;
+
+  boxes:any = {
+    "1": {
+      visible: true
+    },
+    "2": {
+      visible: false
+    },
+    "3": {
+      visible: false
+    },
+    "4": {
+      visible: false
+    },
+    "5": {
+      visible: false
+    }
+  };
+
+  visibleBoxes:number = 0;
+
+  constructor(@Inject(PLATFORM_ID) platformId: string){
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(){
+    this.setBoxVisibleCount()
+  }
+
+  onScatter(){
+    this.setBoxVisible(2);
+    setTimeout(()=>{
+      this.scrollToCard(1)
+    }, 100);
+  }
+
+  onGetToken(){
+    this.setBoxVisible(3);
+    setTimeout(()=>{
+      this.scrollToCard(2)
+    }, 100);
+  }
+
+  onNextCard(id){
+    this.setBoxVisible(id);
+    setTimeout(()=>{
+      this.scrollToCard(id-1)
+    }, 100);
+  }
+
+  scrollToCard(number){
+    this.scrollEl.directiveRef.scrollToX(number * 650, 400)
+  }
+  
+  setBoxVisible(id:number){
+    this.boxes[id].visible = true;
+    this.setBoxVisibleCount();
+  }
+
+  setBoxVisibleCount(){
+    this.visibleBoxes = 0;
+    for (let i in this.boxes){
+      if (this.boxes.hasOwnProperty(i)) {
+        if(this.boxes[i].visible){
+          this.visibleBoxes++;
+        }
+      }
+    }
+  }
+
 }
